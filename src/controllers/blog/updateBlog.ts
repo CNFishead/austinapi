b59e59eeebf037ecc35f2a02f517436c5fbea258
Austin Howard <76625097+CNFishead@users.noreply.github.com>
2023-09-25 15:26:41 -0400

    updated blog controller

--- src/controllers/blog/updateBlog.ts
+++ src/controllers/blog/updateBlog.ts
@@ -12,12 +12,21 @@ import Blog from "../../models/Blog";
  * @author Austin Howard
  * @version 1.0
  * @since 1.0
- * @lastUpdated 2023-09-23T21:02:58.000-05:00
+ * @lastUpdated 2023-09-25T14:52:50.000-05:00
  */
 export default asyncHandler(async (req: AuthenticatedRequest, res: Response, next: any) => {
   try {
     const { id } = req.params!;
     if (!id) return res.status(400).json({ success: false, message: "No id provided" });
+    // find the blog
+    const blog = await Blog.findById(id);
+    if (!blog) return res.status(404).json({ success: false, message: "No blog found" });
+
+    // check if the blog is moving from draft to published, if so, set the published date, but only if there isnt a published date already
+    // this keeps the published date from being overwritten if the blog is updated after it has been published
+    // check the body for whats coming in vs, whats on the blog already
+    if (req.body.isPublished && !blog.publishedAt) req.body.publishedAt = new Date();
+
     const updatedBlog = await Blog.findByIdAndUpdate(id, req.body, { new: true });
     if (!updatedBlog) return res.status(404).json({ success: false, message: "No blog found" });
     return res.status(200).json({ success: true, blog: updatedBlog });
